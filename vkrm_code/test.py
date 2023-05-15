542020206fff3498e863d9020c5937a3d5b61031
Alexander Litvinov <aalitvinov_2@edu.hse.ru>
2023-05-15 22:48:12 +0200

    done finale

--- vkrm_code/test.py
+++ vkrm_code/test.py
@@ -1,6 +1,5 @@
 import polars as pl
 
-df = pl.read_parquet(
-    "./data/ycharts/operating_margin_ttm_parqs/ABEV.parquet", use_pyarrow=True
-)
+df = pl.read_parquet("./data/ycharts/operating_margin_ttm_parqs/ABEV.parquet", use_pyarrow=True)
 print(df)
+
